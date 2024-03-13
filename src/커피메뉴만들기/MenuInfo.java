da4262849c2ed9ee6d4db839ee3bb06209df900a
user1 <98tmddyddl@gmail.com>
2024-03-13 17:44:07 +0900

    유승용 3월11일

--- src/커피메뉴만들기/MenuInfo.java
+++ src/커피메뉴만들기/MenuInfo.java
@@ -2,11 +2,11 @@ package 커피메뉴만들기;
 
 public class MenuInfo {
     String name;
-    int price;
+    String price;
     String category;
     String desc;
 
-    public MenuInfo(String name, int price, String category, String desc) {
+    public MenuInfo(String name, String price, String category, String desc) {
         this.name = name;
         this.price = price;
         this.category = category;
